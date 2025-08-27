e2f05fe7b720b286f40c0c4259079f25a1649782
Manish Shetty <manishshetty827@gmail.com>
2025-08-27 20:54:22 +0530

    Add about page design and GSAP scrollTrigger animation

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
-let tl=gsap.timeline()
-tl.from(".front",{
-    y:1000,
-    duration:1
-})
-tl.from("#coffee",{
-    y:800,
-    duration:1,
-    scale:2
-})
-tl.from(".front h1",{
-    y:1000,
-    duration:1
-})
-tl.from("#coffeeBean1",{
-    y:800,
-    duration:1,
-    scale:2
-})
-tl.from("#coffeeBean2",{
-    y:800,
-    duration:1,
-    scale:2
-})
-tl.from("#coffeeBean3",{
-    y:800,
-    duration:1,
-    scale:2
-})
-tl.from("#coffeeBean4",{
-    y:800,
-    duration:1,
-    scale:2
-})
-tl.from("#coffeeBean5",{
-    y:800,
-    duration:1,
-    scale:2
-})
+let tl = gsap.timeline();
+tl.from(".front", {
+  y: 1000,
+  duration: 1,
+});
+tl.from("#coffee", {
+  y: 800,
+  duration: 1,
+  scale: 2,
+});
+tl.from(".front h1", {
+  y: 1000,
+  duration: 1,
+});
+tl.from("#coffeeBean1", {
+  y: 800,
+  duration: 1,
+  scale: 2,
+});
+tl.from("#coffeeBean2", {
+  y: 800,
+  duration: 1,
+  scale: 2,
+});
+tl.from("#coffeeBean3", {
+  y: 800,
+  duration: 1,
+  scale: 2,
+});
+tl.from("#coffeeBean4", {
+  y: 800,
+  duration: 1,
+  scale: 2,
+});
+tl.from("#coffeeBean5", {
+  y: 800,
+  duration: 1,
+  scale: 2,
+});
+
+let tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#about",
+    scroller: "body",
+    scrub: 2,
+    markers:true,
+    start:"0% 90%",
+    end:"50% 50%"
+  },
+});
+
+tl2.to("#coffee", {
+  top: 615,
+  left: 100,
+  scale:0.9
+},"cb");
+
+tl2.to("#coffeeBean1",{
+  top: 601,
+  left: 218,
+  scale:0.9,
+  rotate:142
+},"cb")
