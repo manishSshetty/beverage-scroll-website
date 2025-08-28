6a9db4457baf454e1a962624bb5fe08455a76538
Manish Shetty <manishshetty827@gmail.com>
2025-08-28 11:00:53 +0530

    Add menu page design and GSAP animations

--- script.js
+++ script.js
@@ -61,3 +61,37 @@ tl2.to("#coffeeBean1",{
   scale:0.9,
   rotate:142
 },"cb")
+
+let tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#menu",
+    scroller: "body",
+    scrub: 2,
+    markers:true,
+    start:"0% 90%",
+    end:"50% 50%"
+  },
+});
+
+tl3.to("#coffee", {
+  top: 1204,
+  left: 525,
+  scale:0.9
+,},"cb");
+
+tl3.to("#coffeeBean1",{
+  top: 1192,
+  left: 415,
+  scale:0.9,
+  rotate:211
+},"cb")
+
+tl3.from("#coffee1",{
+  x:-300,
+  duration:1,
+},"cb")
+
+tl3.from("#coffee2",{
+  x:300,
+  duration:1,
+},"cb")
